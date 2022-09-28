339117eb1bafb37dc1514593c48682c5fa66012b
rosediaz <rosa.diaz.aguilar@gmail.com>
2022-09-27 20:38:32 -0500

    se crea archivo server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 import http from 'http';
 import api from './api/api.js';
+import config from './config/index.js';
+import init from './config/database.js';
 
 const server = http.createServer(api);
 
-server.listen(3000);
+server.on('listening', () => {
+  console.info('Servidor ejecutándose en el puerto', config.server.port);
+});
+
+server.on('error', (e) => {
+  console.error('Error al ejecutar el servidor');
+  if (e.code === 'EADDRINUSE') {
+    console.error('El puerto', config.server.port, 'ya está en uso');
+  }
+});
+
+server.listen(config.server.port);
+init();
+
