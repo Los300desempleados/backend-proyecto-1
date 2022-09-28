e20d245dc60411e9a32c04c4bef0631a1d144d1f
Paco Menchaca <pacocoder86@outlook.com>
2022-09-27 21:11:57 -0500

    feat/FM-16/registered-user-with-encrypted-password

---
+++ api/models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+  dni: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+  },
+  birthDate: {
+    type: Date,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'employee', 'customer'],
+    defaut: 'employee',
+    require: true,
+  },
+  phone: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  alias: {
+    type: String,
+    required: true,
+  }
+});
+
+export default mongoose.model('User', userSchema)
