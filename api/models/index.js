732951cc38d0a1a273e0e6b09672554e6c5cc9f8
servo pcerda <fernandonando98@live.com.mx>
2022-09-28 20:58:06 -0500

    add to index and relationship

--- api/models/index.js
+++ api/models/index.js
@@ -2,5 +2,6 @@ import Movie from './Movie.js'
 import User from './User.js'
 import Projection from './Projection.js'
 import Comments from './Comments.js'
+import Sale from './Sale.js'
 
-export { User, Movie, Projection, Comments }
+export { User, Movie, Projection, Comments, Sale }
