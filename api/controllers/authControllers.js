6591c0e3ea4bcd0646938558ad62ec62fb641573
servo pcerda <fernandonando98@live.com.mx>
2022-09-28 21:01:43 -0500

    login done and tested

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,31 +1,32 @@
-import { User } from "../models/index.js";
-import bcrypt from 'bcrypt';
-
+import { User } from '../models/index.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jwt-simple'
+import config from '../config/index.js'
 
 const register = async (req, res) => {
   try {
     //Encripto contraseña
-    const encrypted = await bcrypt.hash(req.body.password, 10);
+    const encrypted = await bcrypt.hash(req.body.password, 10)
 
     //Modifico el body para remplazar el pass con una encriptada
-    req.body.password = encrypted;
+    req.body.password = encrypted
 
     //Creamos un usuario
-    const user = await User.create(req.body);
+    const user = await User.create(req.body)
 
     //Borramos el password para no mandarla en la respuesta
-    user.password = undefined;
+    user.password = undefined
 
     return res.json({
       msg: 'Usuario registrado exitosamente',
-      user,
-    });
+      user
+    })
   } catch (error) {
     res.status(500).json({
       msg: 'Error al registrar usuario',
-      error,
-    });
+      error
+    })
   }
-};
+}
 
-export { register };
+export { register }
