a051665c6d1e55d45f89a57bd1ab319e61d89ac7
servo pcerda <fernandonando98@live.com.mx>
2022-09-29 21:46:17 -0500

    fix git

--- api/routes/ProjectionRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { ProjectionController } from '../controllers/index.js'
-
-const router = Router()
-
-router.post('/projection', ProjectionController.create)
-export default router
